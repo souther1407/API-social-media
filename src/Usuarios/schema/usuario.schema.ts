f84f12db97bd649b13d54354739bd51892d7fa39
Eric Casanova <kilbane1407@gmail.com>
2022-05-19 16:18:31 -0300

    avatar-default-added

--- src/Usuarios/schema/usuario.schema.ts
+++ src/Usuarios/schema/usuario.schema.ts
@@ -5,7 +5,9 @@ const UsuarioSchema = new Schema({
     nombre: { type:SchemaTypes.String, unique:true},
     email: { type:SchemaTypes.String, unique:true},
     salt: SchemaTypes.String,
-    avatar: SchemaTypes.String
+    avatar: {
+        type:SchemaTypes.String,
+        default:"https://as01.epimg.net/meristation/imagenes/2022/04/27/noticias/1651090407_472545_1651090465_sumario_normal.jpg"}
 })
 
 export default UsuarioSchema
