7261bd6447e7e84ced0c17f0d39ec392bfcf9fc4
Eric Casanova <kilbane1407@gmail.com>
2022-05-23 16:38:20 -0300

    img prueba 1

--- src/utils/files.utiles.ts
+++ src/utils/files.utiles.ts
@@ -1,10 +1,11 @@
 import {UploadedFile} from "express-fileupload"
-
+import x from "../static/imagenes"
 function generarNumeroAleatorio(){
     return Math.floor(Math.random()*2342398923589)
 }
 
 export async function guardarImagen(imagen:UploadedFile){
     imagen.name="imagen"+generarNumeroAleatorio()+".png"
-    await imagen.mv("../static/imagenes")
+    console.log(__dirname)
+    await imagen.mv("../static/imagenes/"+imagen.name)
 }
