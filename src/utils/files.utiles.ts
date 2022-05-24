edb837bb0fa62328f45236bc4b5062856ec1f793
Eric Casanova <kilbane1407@gmail.com>
2022-05-23 23:40:53 -0300

    cambiado algoritmo de num aleatorio

--- src/utils/files.utiles.ts
+++ src/utils/files.utiles.ts
@@ -1,7 +1,7 @@
 import {UploadedFile} from "express-fileupload"
 
 function generarNumeroAleatorio(){
-    return Math.floor(Math.random()*2342398923589)
+    return Math.floor(Math.random()*Number.MAX_SAFE_INTEGER)
 }
 
 export async function guardarImagen(imagen:UploadedFile){
